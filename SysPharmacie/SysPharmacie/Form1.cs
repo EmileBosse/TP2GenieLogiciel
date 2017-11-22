ebefcf813f1f0cec833b4e601d6fffd5afa52ee6
Émile Bossé <emile.bosse@cidco.ca>
2017-11-22 03:00:10 -0500

    ajout interface Wizzard

--- SysPharmacie/SysPharmacie/Form1.cs
+++ SysPharmacie/SysPharmacie/Form1.cs
@@ -16,5 +16,11 @@ namespace SysPharmacie
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WIZZ01 c = new WIZZ01();
+            c.Show();
+        }
     }
 }
