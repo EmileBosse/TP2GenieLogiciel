993c22e003937d9c8cbbafce37ba245d955b290f
WillisCoffee <wthibodeau@hotmail.com>
2017-11-21 19:48:08 -0600

    Ajut de PRES01

---
+++ SysPharmacie/SysPharmacie/PRES01.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace SysPharmacie
+{
+    partial class PRES01
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtNomMedicament = new System.Windows.Forms.TextBox();
+            this.txtNumeroMedicament = new System.Windows.Forms.TextBox();
+            this.lblNom = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAjouterMedicament = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.textBox5);
+            this.panel2.Controls.Add(this.listBox2);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.textBox3);
+            this.panel2.Controls.Add(this.txtNomMedicament);
+            this.panel2.Controls.Add(this.txtNumeroMedicament);
+            this.panel2.Controls.Add(this.lblNom);
+            this.panel2.Controls.Add(this.lblNumero);
+            this.panel2.Location = new System.Drawing.Point(345, 174);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(448, 323);
+            this.panel2.TabIndex = 62;
+            // 
+            // txtNomMedicament
+            // 
+            this.txtNomMedicament.Location = new System.Drawing.Point(98, 45);
+            this.txtNomMedicament.Name = "txtNomMedicament";
+            this.txtNomMedicament.Size = new System.Drawing.Size(100, 20);
+            this.txtNomMedicament.TabIndex = 41;
+            // 
+            // txtNumeroMedicament
+            // 
+            this.txtNumeroMedicament.Location = new System.Drawing.Point(98, 19);
+            this.txtNumeroMedicament.Name = "txtNumeroMedicament";
+            this.txtNumeroMedicament.Size = new System.Drawing.Size(70, 20);
+            this.txtNumeroMedicament.TabIndex = 40;
+            // 
+            // lblNom
+            // 
+            this.lblNom.AutoSize = true;
+            this.lblNom.Location = new System.Drawing.Point(61, 48);
+            this.lblNom.Name = "lblNom";
+            this.lblNom.Size = new System.Drawing.Size(30, 13);
+            this.lblNom.TabIndex = 35;
+            this.lblNom.Text = "Date";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Location = new System.Drawing.Point(47, 22);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(44, 13);
+            this.lblNumero.TabIndex = 34;
+            this.lblNumero.Text = "Numéro";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Location = new System.Drawing.Point(345, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(381, 142);
+            this.panel1.TabIndex = 61;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 99);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 44;
+            this.button1.Text = "Rechercher";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 42;
+            this.label1.Text = "Numéro";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(98, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 20);
+            this.textBox1.TabIndex = 40;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "1\t2017-01-01,",
+            "2 \t2017-02-02"});
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(308, 537);
+            this.listBox1.TabIndex = 60;
+            // 
+            // btnAjouterMedicament
+            // 
+            this.btnAjouterMedicament.Location = new System.Drawing.Point(625, 503);
+            this.btnAjouterMedicament.Name = "btnAjouterMedicament";
+            this.btnAjouterMedicament.Size = new System.Drawing.Size(168, 43);
+            this.btnAjouterMedicament.TabIndex = 63;
+            this.btnAjouterMedicament.Text = "Entrer prescription";
+            this.btnAjouterMedicament.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(61, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 43;
+            this.label2.Text = "Date";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(98, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(208, 20);
+            this.textBox2.TabIndex = 41;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(98, 71);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(208, 20);
+            this.textBox3.TabIndex = 42;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 43;
+            this.label3.Text = "Patient";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 123);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 13);
+            this.label4.TabIndex = 44;
+            this.label4.Text = "Médicaments";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "Acétaminophène\t3/jour\t7 jours\t2 renouv (1 restant),",
+            "Ibuprophène\t1/jour\t30 jours\t3 renouv (0 restant)"});
+            this.listBox2.Location = new System.Drawing.Point(97, 123);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(335, 186);
+            this.listBox2.TabIndex = 45;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(51, 76);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 46;
+            this.label5.Text = "Patient";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(98, 73);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(208, 20);
+            this.textBox4.TabIndex = 45;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(48, 13);
+            this.label6.TabIndex = 47;
+            this.label6.Text = "Médecin";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(98, 97);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(208, 20);
+            this.textBox5.TabIndex = 46;
+            // 
+            // PRES01
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(805, 560);
+            this.Controls.Add(this.btnAjouterMedicament);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "PRES01";
+            this.Text = "PRES01";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtNomMedicament;
+        private System.Windows.Forms.TextBox txtNumeroMedicament;
+        private System.Windows.Forms.Label lblNom;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAjouterMedicament;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+    }
+}
